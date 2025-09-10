70c08b2b1f4459d3a199439320623d79cddfe034
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-10 09:42:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express =require('express')
 const app=express()
 const PORT = process.env.PORT
-
+app.use(express.json());
 app.get('/',(req,res)=>{
   res.send(`.env 테스트:${process.env.DATABASE_NAME}`)
 })
